5b89a5bb7e74ab4b7edf79d2bbd8194dac5aa44a
mohamedelmouden <m.elmouden1908@uca.ac.ma>
2026-03-10 00:53:14 +0000

    Add personal message to production version log

--- src/main/java/dao/DaoImpl.java
+++ src/main/java/dao/DaoImpl.java
@@ -1,14 +1,14 @@
 package dao;
-
+// message personnelle::0637
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-
+// message personnelle::0637
 @Component("dao")
 @Profile("prod")  // Ce bean sera actif uniquement avec le profil "prod"
 public class DaoImpl implements IDao {
     @Override
     public double getValue() {
-        System.out.println("Version de production");
+        System.out.println("Version de production");// message personnelle::0637
         return 100.0;
     }
 }
