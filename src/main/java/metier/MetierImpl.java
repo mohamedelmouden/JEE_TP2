55063e38b1612621af68f6424003138513f4fa03
mohamedelmouden <m.elmouden1908@uca.ac.ma>
2026-03-10 00:54:26 +0000

    Refactor MetierImpl to include personal message comments

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,23 +1,23 @@
 package metier;
-
+// message personnelle::0637
 import dao.IDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
+// message personnelle::0637
 @Component("metier")
 public class MetierImpl implements IMetier {
-
+// message personnelle::0637
     @Autowired
     @Qualifier("dao2")  // Spécifie quelle implémentation de IDao utiliser
     private IDao dao;
-
+// message personnelle::0637
     @Override
     public double calcul() {
         return dao.getValue() * 2;
     }
-
+// message personnelle::0637
     public void setDao(IDao dao) {
         this.dao = dao;
-    }
-}
+    }// message personnelle::0637
+}// message personnelle::0637
